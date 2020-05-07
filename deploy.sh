881c450d7bae10dcd16109412e5841ff9394bb10
Javier Estupinan <jestupinan@zeerbyte.com>
2020-05-07 11:42:04 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jaec86/amt-workshop1.git master:gh-pages
+git push -f git@github.com:jaec86/amt-workshop3.git master:gh-pages
 
 # cd -
